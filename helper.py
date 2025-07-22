6f7aad6b39923ea10b7e8722e1d031732905f62b
mbakir <mbakir@i-tracing.com>
2025-07-22 19:05:04 +0200

    added first training part

--- helper.py
+++ helper.py
@@ -2,17 +2,20 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class GroupNorm(nn.Module):
     def __init__(self, channels):
         super(GroupNorm, self).__init__()
         self.gn = nn.GroupNorm(num_groups=32, num_channels=channels, eps=1e-6, affine=True)
-    
+
     def forward(self, x):
         return self.gn(x)
-    
+
+
 class Swish(nn.Module):
     def forward(self, x):
         return x * torch.sigmoid(x)
+
 
 class ResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
@@ -20,42 +23,45 @@ class ResidualBlock(nn.Module):
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.block = nn.Sequential(
-                                   GroupNorm(in_channels),
-                                   Swish(),
-                                   nn.Conv2d(in_channels,out_channels,3,1,1),
-                                   GroupNorm(out_channels),
-                                   Swish(),
-                                   nn.Conv2d(out_channels,out_channels,3,1,1)
-                                   )
-        
+            GroupNorm(in_channels),
+            Swish(),
+            nn.Conv2d(in_channels, out_channels, 3, 1, 1),
+            GroupNorm(out_channels),
+            Swish(),
+            nn.Conv2d(out_channels, out_channels, 3, 1, 1)
+        )
+
         if in_channels != out_channels:
-            self.channel_up = nn.Conv2d(in_channels,out_channels, 1, 1, 0)
-            
+            self.channel_up = nn.Conv2d(in_channels, out_channels, 1, 1, 0)
+
     def forward(self, x):
         if self.in_channels != self.out_channels:
             return self.channel_up(x) + self.block(x)
         else:
             return x + self.block(x)
-        
+
+
 class UpSampleBlock(nn.Module):
     def __init__(self, channels):
-        super(UpSampleBlock, self).__init()
-        self.conv == nn.Conv2d(channels, channels, 3, 1, 1)
-        
+        super(UpSampleBlock, self).__init__()
+        self.conv = nn.Conv2d(channels, channels, 3, 1, 1)
+
     def forward(self, x):
-        x = F.interpolate(x, scale_factor= 2.0)
+        x = F.interpolate(x, scale_factor=2.0)
         return self.conv(x)
-    
+
+
 class DownSampleBlock(nn.Module):
     def __init__(self, channels):
-        super(DownSampleBlock, self).__init()
-        self.conv == nn.Conv2d(channels, channels, 3, 2, 0)
-        
+        super(DownSampleBlock, self).__init__()
+        self.conv = nn.Conv2d(channels, channels, 3, 2, 0)
+
     def forward(self, x):
         pad = (0, 1, 0, 1)
-        x = F.pad(x, pad, mode="constant", value = 0)
+        x = F.pad(x, pad, mode="constant", value=0)
         return self.conv(x)
-    
+
+
 class NonLocalBlock(nn.Module):
     def __init__(self, channels):
         super(NonLocalBlock, self).__init__()
@@ -89,3 +95,14 @@ class NonLocalBlock(nn.Module):
         A = A.reshape(b, c, h, w)
 
         return x + A
+
+
+
+
+
+
+
+
+
+
+
